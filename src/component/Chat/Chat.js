2fbba06517fd7203f46352882032c1a71177e177
MuskanMandloi <54906865+muskanmandloi@users.noreply.github.com>
2023-09-08 23:16:40 +0530

    Update Chat.js

--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -9,7 +9,7 @@ import closeIcon from "../../images/closeIcon.png";
 
 let socket;
 
-const ENDPOINT = "";
+const ENDPOINT = "https://message-backend-app.onrender.com";
 
 const Chat = () => {
     const [id, setid] = useState("");
